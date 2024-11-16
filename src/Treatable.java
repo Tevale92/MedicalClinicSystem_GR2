3a2851f956c2a99749829a328f48a7def09e5927
Tevale92 <daviddemers92@gmail.com>
2024-11-16 17:38:28 -0500

    MCSv1.3

--- src/Treatable.java
+++ src/Treatable.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
+
+/**
+ * Treatable interface which declares two methods.
+ * The performTreatment method is to add a Treatment to the list of treatments.
+ * The treatmentDescs is a getter method to return the list of treatments.
+ * @version 1.3
+ * @since 11/15/2024
+ * @author Louis Chartier, Rima Dagher and David Demers
+ */
+
 interface Treatable {
 
-    public Treatment performTreatment();
+    public void performTreatment(Treatment treatment);
+
+    public ArrayList<Treatment> treatmentDescs();
 }
